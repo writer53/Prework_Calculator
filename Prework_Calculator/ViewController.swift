74bc6a7bd3d344c28d29c723f6cfbc897ced585b
Writer Leto <wpl19a@my.fsu.edu>
2021-08-30 18:22:06 -0400

    Initial Commit

---
+++ Prework_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework_Calculator
+//
+//  Created by user203102 on 8/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Prework_CalculatorTests/Prework_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  Prework_CalculatorTests.swift
+//  Prework_CalculatorTests
+//
+//  Created by user203102 on 8/30/21.
+//
+
+import XCTest
+@testable import Prework_Calculator
+
+class Prework_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
